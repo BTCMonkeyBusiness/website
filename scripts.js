6c0936868d1ecb268e1a96fb436b33b7287d51ea
BTCMonkeyBusiness <129183289+BTCMonkeyBusiness@users.noreply.github.com>
2023-03-28 19:11:47 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,17 @@
 /* scripts.js */
 const pageSize = 100;
-const pageCount = 100;
+const pageCount = 50;
 const thumbnails = document.getElementById('thumbnails');
-let currentPage = 1;
+const pageSlider = document.getElementById('pageSlider');
+const pageNumber = document.getElementById('pageNumber');
+let currentPage = 0;
 
 function renderThumbnails(page) {
-    const startIndex = (page - 1) * pageSize + 1;
-    const endIndex = Math.min(startIndex + pageSize - 1, 10000);
+    thumbnails.innerHTML = ''; // Ajouter cette ligne pour nettoyer les vignettes existantes
+    const startIndex = page * pageSize + 1;
+    const endIndex = Math.min(startIndex + pageSize - 1, 5000);
     for (let i = startIndex; i <= endIndex; i++) {
+
         const column = document.createElement('div');
         column.classList.add('column');
         const img = document.createElement('img');
@@ -26,20 +30,35 @@ function renderThumbnails(page) {
     }
 }
 
-function loadNextPage() {
-    if (currentPage < pageCount) {
-        currentPage++;
+function goToPage(page) {
+    if (page >= 0 && page < pageCount) { // Modifier la condition pour inclure la valeur 0
+        currentPage = page;
+        pageNumber.textContent = currentPage;
         renderThumbnails(currentPage);
     }
 }
 
-window.addEventListener('scroll', () => {
-    if (document.documentElement.scrollTop + window.innerHeight >= document.documentElement.scrollHeight) {
-        loadNextPage();
-    }
+pageSlider.addEventListener('input', () => {
+    goToPage(parseInt(pageSlider.value));
 });
 
+function loadNextPage() {
+    goToPage(currentPage + 1);
+}
+
+function loadPreviousPage() {
+    goToPage(currentPage - 1);
+}
+
+// Supprimer cet événement d'écoute pour désactiver le chargement automatique des images lors du défilement
+// window.addEventListener('scroll', () => {
+//     if (document.documentElement.scrollTop + window.innerHeight >= document.documentElement.scrollHeight) {
+//         loadNextPage();
+//     }
+// });
+
 renderThumbnails(currentPage);
+
 
 const urlParams = new URLSearchParams(window.location.search);
 const searchQuery = urlParams.get('search');
@@ -61,6 +80,11 @@ function search() {
     if (isNaN(index)) {
         return;
     }
+    if (index > 5000) {
+        alert("Veuillez entrer un nombre inférieur ou égal à 5000.");
+        return;
+    }
+
     const page = Math.ceil(index / pageSize);
     if (page !== currentPage) {
         thumbnails.innerHTML = '';
